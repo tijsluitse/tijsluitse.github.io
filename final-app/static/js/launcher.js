3b8d0045a176b2753297cbc07297506fd2feba53
Tijs Luitse - Imac <tijs.luitse@gmail.com>
2016-02-16 17:55:59 +0100

    New setup

---
+++ final-app/static/js/launcher.js
@@ -0,0 +1,372 @@
+/////////////////
+// Information //
+/////////////////
+
+// Created by: Tijs Luitse
+// Cooperation: Tom Snepvangers
+// Minor: Web application from scratch
+
+///////////////
+// Libraries //
+///////////////
+
+// JSON: ./libs/aja.min.js
+// Routing: ./libs/routie.js
+// Shake gesture: ./libs/shake.js
+// Templating: ./libs/transparency.min.js
+// Map/filter/reduce: ./libs/underscore-min.js
+
+///////////////
+// Variables //
+///////////////
+
+var allSections = document.querySelectorAll('section');
+var searchInput = document.getElementById('search');
+var searchSubmit = document.getElementById('submit');
+var photoGalleryTarget = document.getElementById('photoGallery');
+var popularPostsTarget = document.getElementById('popularPosts');
+var singlePhotoTarget = document.getElementById('singlePhoto');
+var userInfoTarget = document.getElementById('userInfo');
+var userFeedTarget = document.getElementById('userFeed');
+var errorMessageTarget = document.getElementById('errorMessage');
+
+
+/////////////////
+// Application //
+/////////////////
+
+(function() {
+	'use strict'
+
+	var launcher = {
+
+		init: function(){
+			routes.init();
+			photoGallery.searchFunction();
+		}
+
+	};
+
+	var routes = {
+
+		init: function() {
+
+			routie({
+
+				// Landingspage with recent instagram posts
+			    'landing': function() {
+			    	photoGallery.popularPosts();
+			    	sections.toggle(this.path);
+			    },
+
+			    // // 
+			    // 'instaPosts': function() {
+			    // 	sections.toggle(this.path);
+			    // },
+
+			    // Single user
+			    'single/:id': function(photoId) {
+			    	photoGallery.singlePhoto(photoId);
+			   	 	sections.toggle('singlePhoto');
+			    },
+
+			    // Single user
+			    'user/:username': function(userId){
+			    	single.userInfo(userId);
+			    	single.userFeed(userId);
+			    	sections.toggle('singleUser');
+			    }
+
+			});
+		}
+	}
+
+	var sections = {
+
+		toggle: function(hashName) {
+
+			var section = document.getElementById(hashName);
+
+			for (var i = 0; i < allSections.length; i++) {
+				allSections[i].classList.remove('active');
+			};
+
+			section.classList.toggle('active');
+
+		}
+
+	}
+
+	var photoGallery = {
+
+		popularPosts: function() {
+
+			aja()
+				.url('https://api.instagram.com/v1/media/popular?access_token=806401368.5aa13be.4a08df065cbb41469c9cc20041432d3b')
+			    .type('jsonp')
+			    .cache('false')
+			    .on('success', function(data){			    
+			    	
+			    	var data = data.data;
+			   
+			    	var filteredData = _.map(data, function(photoInfo){
+			    		return _.pick(photoInfo, 'id', 'link', 'likes', 'user', 'images', 'tags', 'title');
+			    	});
+
+			    	data = filteredData;
+
+			    	console.log(data);
+			    	
+			        var directives = {
+			      			       
+			        	photoLink: {
+			        		href: function(params) {
+			        			return '#single/' + this.id;			        		
+			        		}
+			        	},
+			        	photoImage: {
+			        		src: function(params) {
+			        			return this.images.low_resolution.url;
+			        		}			        	
+			        	},
+			        	photoTags: {
+			        		text: function(params) {
+			        			return this.tags;
+			        		}
+			        	}
+			        	
+					}
+
+					Transparency.render(popularPostsTarget, data, directives);
+
+			    })
+
+			.go();
+
+		},
+
+		searchFunction: function(){
+
+			searchSubmit.addEventListener('click', function(){
+				var tag = searchInput.value;
+				photoGallery.searchResults(tag);
+			});
+
+		},
+
+		searchResults: function(tag) {
+			
+			aja()
+				.url('https://api.instagram.com/v1/tags/' + tag + '/media/recent?access_token=806401368.5aa13be.4a08df065cbb41469c9cc20041432d3b')
+			    .type('jsonp')
+			    .cache('false')
+			    .on('success', function(data){			    
+			    	
+			    	var data = data.data;
+			   
+			    	var filteredData = _.map(data, function(photoInfo){
+			    		return _.pick(photoInfo, 'id', 'link', 'likes', 'user', 'images', 'tags', 'title');
+			    	});
+
+			    	data = filteredData;
+
+			    	console.log(data);
+
+			    	if (data.length < 1) {
+
+			    		photoGallery.noResults(tag);
+
+			    	} else {
+
+			    		var directives = {
+			      			       
+			        	photoLink: {
+			        		href: function(params) {
+			        			return '#single/' + this.id;			        		
+			        		}
+			        	},
+			        	photoImage: {
+			        		src: function(params) {
+			        			return this.images.low_resolution.url;
+			        		}			        	
+			        	},
+			        	photoLikes: {
+			        		text: function(params) {
+			        			return 'Likes: ' + this.likes.count;
+			        		}
+			        	},
+			        	photoUser: {
+			        		href: function(params) {
+			        			return '#user/' + this.user.id;
+			        		},
+			        		text: function(params) {
+			        			return 'User: ' + this.user.full_name;
+			        		}
+			        	}
+			        	
+					}
+
+					Transparency.render(photoGalleryTarget, data, directives);
+
+			    	}
+
+			    })
+
+			.go();
+		},
+
+		noResults: function(tag) {
+
+			console.log("Tag not found");
+
+			var directives = {
+			      			       
+	        	error: {
+	        		text: function(params) {
+	        			return 'There are no photos tagged with: ' + tag + '.';
+	        		}
+	        	}
+	        	
+			}
+
+			Transparency.render(errorMessageTarget, tag, directives);
+
+		},
+
+		singlePhoto: function(photoId) {
+
+			aja()
+				.url('https://api.instagram.com/v1/media/' +  photoId + '?access_token=806401368.5aa13be.4a08df065cbb41469c9cc20041432d3b')
+			    .type('jsonp')
+			    .cache('false')
+			    .on('success', function(data){			    
+			    	
+			    	var data = data.data;
+
+			    	console.log(data);
+
+			        var directives = {
+
+			      		photoTitle: {
+			      			text: function(params) {
+			      				return this.caption.text;
+			      			}
+			      		},	       
+			        	photoImage: {
+			        		src: function(params) {
+			        			return this.images.standard_resolution.url;
+			        		}			        	
+			        	},
+			        	photoLikes: {
+			        		text: function(params) {
+			        			return 'Likes: ' + this.likes.count;
+			        		}
+			        	},
+			        	photoUser: {
+			        		href: function(params) {
+			        			return '#user/' + this.user.id;
+			        		},
+			        		text: function(params) {
+			        			return 'User: ' + this.user.full_name;
+			        		}
+			        	}
+			        	
+					}
+
+					Transparency.render(singlePhotoTarget, data, directives);
+
+			    })
+
+			.go();
+
+		}
+	
+	}
+
+	var single = {
+
+		userInfo: function(userId) {
+			
+			aja()
+				.url('https://api.instagram.com/v1/users/'  +  userId + '?access_token=806401368.5aa13be.4a08df065cbb41469c9cc20041432d3b')
+			    .type('jsonp')
+			    .cache('false')
+			    .on('success', function(data){			    
+			    	
+			    	var data = data.data;
+
+			    	console.log(data);
+
+			        var directives = {
+
+			      		userPicture: {
+			      			src: function(params) {
+			      				return this.profile_picture;
+			      			}
+			      		},	       
+			        	userName: {
+			        		text: function(params) {
+			        			return this.full_name;
+			        		}			        	
+			        	},
+			        	userBio: {
+			        		text: function(params) {
+			        			return this.bio;
+			        		}
+			        	}
+			        	
+					}
+
+					Transparency.render(userInfoTarget, data, directives);
+
+			    })
+
+			.go();
+
+		},
+
+		userFeed: function(userId) {
+	
+			aja()
+				.url('https://api.instagram.com/v1/users/'  +  userId + '/media/recent/?access_token=806401368.5aa13be.4a08df065cbb41469c9cc20041432d3b')
+			    .type('jsonp')
+			    .cache('false')
+			    .on('success', function(data){			    
+			    	
+			    	var data = data.data;
+
+			    	console.log(data);
+
+			        var directives = {
+
+			      		photoLink: {
+			        		href: function(params) {
+			        			return '#single/' + this.id;			        		
+			        		}
+			        	},
+			        	photoImage: {
+			        		src: function(params) {
+			        			return this.images.low_resolution.url;
+			        		}			        	
+			        	},
+			        	photoTags: {
+			        		text: function(params) {
+			        			return this.tags;
+			        		}
+			        	}
+			        	
+					}
+
+					Transparency.render(document.getElementById('feedItems'), data, directives);
+
+			    })
+
+			.go();			
+
+		}
+
+	}
+
+	launcher.init();
+
+})();
